e2a8dfcdd12fec5f513667fe2da3e4345cc543e0
Avijit Maiti <avijit.haldia@gmail.com>
2021-08-19 12:27:30 +0530

    Using valid Movie Id

--- ratings-data-service/ratings-data-service/src/main/java/io/javabrains/ratingsdataservice/resource/RatingsResource.java
+++ ratings-data-service/ratings-data-service/src/main/java/io/javabrains/ratingsdataservice/resource/RatingsResource.java
@@ -24,8 +24,8 @@ public class RatingsResource {
 	public UserRating getUserRating(@PathVariable("userID") String userID) {
 		
 		List <Rating> ratings = Arrays.asList(
-				new Rating("1234", 4),
-				new Rating("5678", 3)
+				new Rating("100", 4),
+				new Rating("550", 3)
 				);
 		UserRating userRating = new UserRating();
 		userRating.setUserRating(ratings);
