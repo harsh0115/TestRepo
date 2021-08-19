c9adfe5dc4f56ab7e7a7dec3daf784c2f4a777fc
Avijit Maiti <avijit.haldia@gmail.com>
2021-08-19 12:24:51 +0530

    Replacing with RestTemplateBuilder to set timeout

--- movie-info-service/movie-info-service/src/main/java/io/javabrains/movieinfoservice/MovieInfoServiceApplication.java
+++ movie-info-service/movie-info-service/src/main/java/io/javabrains/movieinfoservice/MovieInfoServiceApplication.java
@@ -2,9 +2,12 @@ package io.javabrains.movieinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -16,7 +19,12 @@ public class MovieInfoServiceApplication {
 	
 	@Bean
 	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+
+//		return new RestTemplate();
+		return new RestTemplateBuilder()
+				.setConnectTimeout(Duration.ofMinutes(3))
+				.setReadTimeout(Duration.ofMinutes(3))
+				.build();
 	}
 
 }
